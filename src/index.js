868bca3b33ac2bee813d15e09c63afbb45bf8d22
Oleksii Bespalyi <skripkinlexxx@gmail.com>
2024-11-04 23:00:38 +0100

    add file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
+import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './contacts/index.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
+  await createDirIfNotExists(TEMP_UPLOAD_DIR);
+  await createDirIfNotExists(UPLOAD_DIR);
   setupServer();
 };
 
-bootstrap();
+void bootstrap();
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,10 @@
-// src/routers/index.js
-
 import { Router } from 'express';
 import contactsRouter from './contacts.js';
-import authRouter from './auth.js';
+import userRouter from './auth.js';
 
 const router = Router();
 
 router.use('/contacts', contactsRouter);
-router.use('/auth', authRouter);
+router.use('/auth', userRouter);
 
 export default router;
